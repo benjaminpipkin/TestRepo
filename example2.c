e60146e9fd2ae74d12bd34f3d8c3e7f58bbc243d
Ben Pipkin <pipkin.benjamin@gmail.com>
2016-10-24 11:23:36 -0400

    updated example2.c

--- example2.c
+++ example2.c
@@ -1,7 +1,15 @@
 // this is a second example
+#include <stdio.h>
+
 int main()
 {
-	char * charPtr = (char *)malloc(1);
+	int size = 20;
+	char * charPtr = (char *)malloc(size);
+	for (int i = 0; i < size; i++)
+	{
+		*(charPtr + i) = 0xF0 + (i % 10);
+	}// end for loop
+	printf("%s",charPtr); // should print '01234567890123456789'
 	free(charPtr);
 	return 0;
 } // end main
